69414717b6d28e451b16fdbc782d3438eabbf8f8
thuytran95 <thuytt0295@gmail.com>
2020-11-24 16:46:33 +0700

    customize News component

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import data from "../../assets/data/news.data.json";
+import "./style.scss";
 
 const News = () => {
   const newsTabList = data;
   console.log(newsTabList);
   return (
-    <section className="news" id="news">
+    <section className="news-section" id="news">
       <ul className="nav nav-tabs hotnews__tab" id="hotnews-tab" role="tablist">
         <li className="nav-item hotnews__tab__item">
           <a
@@ -51,89 +52,74 @@ const News = () => {
         {newsTabList.map((tab, index) => {
           return (
             <div
-              className="tab-pane fade active show"
+              className={
+                index === 0 ? "tab-pane fade active show" : "tab-pane fade"
+              }
               id={tab.id}
               role="tabpanel"
               aria-labelledby={`${tab.id}-tab`}
+              key={tab.id}
             >
               <div className="container">
                 <div className="row">
-                  {tab.newsList.map((newItem, index) => {
-                    if (index < 2) {
+                  {tab.newsList.map((newsItem, index1) => {
+                    // console.log(newsItem.smallNewList);
+                    if (!newsItem.smallNewList) {
                       return (
-                        <div className="col-12 col-sm-6">
-                          <div class="hotnews__item">
-                            <div class="hotnews__item__img">
-                              <a href="#">
-                                <img src="./images/news/da-n-my-nhan-trong-the-gio-i-die-n-a-nh-cu-a-qua-i-kie-t-christopher-nolan-15970503918450.jpg" />{" "}
-                              </a>
-                            </div>
-                            <div class="hotnews__item__content">
-                              <p class="news-title">
-                                <a href="#">
-                                  Dàn mỹ nhân trong thế giới điện ảnh của quái
-                                  kiệt Christopher Nolan
-                                </a>
-                              </p>
-                              <p class="news-description">
-                                Làng phim đương đại những năm qua chứng kiến sự
-                                lên ngôi của cái tên Christopher Nolan, được
-                                biết tới như một trong những đạo diễn thiên tài
-                                với khả năng tạo ra siêu phẩm bạc tỉ chất lượng.
-                              </p>
-                            </div>
-                            <div class="social">
-                              <div class="social-icon social__like">
-                                <img src="./images/icons/like.png" />{" "}
-                                <span>0</span>
-                              </div>
-                              <div class="social-icon socail__comments">
-                                <img src="./images/icons/comment.png" />{" "}
-                                <span>0</span>
-                              </div>
-                            </div>
+                        <div
+                          className="col-news col-xs-12 news"
+                          key={`${tab.id}-${index1}`}
+                        >
+                          <div className="news__image">
+                            <a href="#">
+                              <img
+                                alt="news"
+                                src={`./images/news/${tab.nameTab}/${newsItem.image}`}
+                              />
+                            </a>
                           </div>
-                        </div>
-                      );
-                    } else if (index < 4) {
-                      return (
-                        <div className="col-12 col-sm-4">
-                          <div>
-                            <div className="hotnews__item">
-                              <div className="hotnews__item__img">
-                                <a href="#">
-                                  <img src="./images/news/6-da-o-die-n-ti-do-lam-nen-thanh-cong-cua-nhu-ng-bom-ta-n-di-nh-da-m-nha-t-hollywood-15966023653637.jpg" />{" "}
-                                </a>
-                              </div>
-                              <div className="hotnews__item__content">
-                                <p className="news-title">
-                                  <a href="#">
-                                    6 đạo diễn tỉ đô làm nên thành công của
-                                    những bom tấn đình đám nhất Hollywood
-                                  </a>
-                                </p>
-                                <p className="news-description">
-                                  Hollywood luôn yêu thích những dự án điện ảnh
-                                  với tiềm năng trở thành bom tấn. Trên con
-                                  đường đó, đạo diễn là một trong những vị trí
-                                  quan trọng nhất
-                                </p>
-                              </div>
-                            </div>
-                            <div className="social">
-                              <div className="social-icon social__like">
-                                <img src="./images/icons/like.png" />
-                                <span>0</span>
-                              </div>
-                              <div className="social-icon socail__comments">
-                                <img src="./images/icons/comment.png" />
-                                <span>0</span>
-                              </div>
-                            </div>
-                          </div>
+                          <a className="news__link" href="#">
+                            <p className="news__link__title">
+                              {newsItem.title}
+                            </p>
+                          </a>
+                          <p className="news__description">
+                            {newsItem.description}
+                          </p>
                         </div>
                       );
                     }
+                    return (
+                      <div
+                        className="col-news col-xs-12 news"
+                        key={`${tab.id}-${index1}`}
+                      >
+                        <div className="row">
+                          {newsItem.smallNewList.map((smallNew, index2) => {
+                            return (
+                              <div
+                                className="col-xs-12 col-sm-12"
+                                key={`${tab.id}-${index1}-${index2}`}
+                              >
+                                <div className="news__image">
+                                  <a href="#">
+                                    <img
+                                      alt="news"
+                                      src={`./images/news/${tab.nameTab}/${smallNew.image}`}
+                                    />
+                                  </a>
+                                </div>
+                                <a className="news__link" href="#">
+                                  <p className="news__link__title">
+                                    {smallNew.title}
+                                  </p>
+                                </a>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
                   })}
                 </div>
               </div>
