ae1cf5d9b601ca85d7cdae9fa2be6225d538ccce
Tran Thuy <thuytt0295@gmail.com>
2021-01-20 20:25:00 +0700

    change validation schema

--- src/Service/User.js
+++ src/Service/User.js
@@ -3,15 +3,18 @@ import * as yup from "yup";
 
 // schema do minh tao ra de validation du lieu thu duoc tu form
 export const signUpUserSchema = yup.object().shape({
-  taiKhoan: yup.string().required("*Field is required"),
-  matKhau: yup.string().required("*Field is required"),
-  hoTen: yup.string().required("*Field is required"),
-  email: yup.string().required("*Field is required").email("*Email is invalid"),
+  taiKhoan: yup.string().required("*Trường này bắt buộc nhập"),
+  matKhau: yup.string().required("*Trường này bắt buộc nhập"),
+  hoTen: yup.string().required("*Trường này bắt buộc nhập"),
+  email: yup
+    .string()
+    .required("*Trường này bắt buộc nhập")
+    .email("*Email không hợp lệ"),
   soDt: yup
     .string()
-    .required("*Field is required")
+    .required("*Trường này bắt buộc nhập")
     .matches(/^[0-9]+$/), //viet bieu thuc chinh quy phai viet lien, khong co dau cach giua cac ky tu
-  maNhom: yup.string().required("*Field is required"),
+  maNhom: yup.string().required("*Trường này bắt buộc nhập"),
 });
 
 class UserService {
@@ -29,7 +32,6 @@ class UserService {
       url: "https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/DangNhap",
       data,
     });
-    
   }
   // infomationUser(data) {
   //   return Axios({
@@ -37,11 +39,8 @@ class UserService {
   //     url: "https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThongTinTaiKhoan",
   //     data,
   //   });
-    
-  // }
-  
 
-  
+  // }
 }
 
 export default UserService;
